108ffb9ce3c3ebd5e1aab6e5810c7a38cd24e4c7
Rene <rene.souza@virtualideas.com.br>
2016-11-21 14:06:09 -0200

    seting up server with POST endpoint

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');//take body and convert to an object
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todos');
+const {User} = require('./models/users');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(bodyParser.json()); //body parser middleware
+
+app.post('/todos',(req, res)=>{
+	//console.log(req.body);
+	var todo = new Todo({
+		text: req.body.text
+	});
+	todo.save().then(
+		(doc)=>{
+			res.send(doc);
+		},
+		(e)=>{
+			res.status(400).send(e);
+		}
+	);
+});
+
+app.listen(port, ()=>{
+	console.log(`Server runing at port ${port}`);
+});
